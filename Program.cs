f3da0635362d5baad5f27e45ce2159fc64162312
enlightenedihi <enlightenihi@live.com>
2018-02-18 13:36:17 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS.Oid.Start
+{
+    static class Program
+    {
+        private static string DNA =
+        "using System;" +
+        "namespace Oid" +
+        "{" +
+        "    class Program" +
+        "    {" +
+        "        static void Main(string[] args)" +
+        "        {" +
+        "        }" +
+        "    }" +
+        "}";
+
+        private static void Main()
+        {
+            Console.WriteLine(DNA);
+            Console.ReadLine();
+        }
+    }
+}
